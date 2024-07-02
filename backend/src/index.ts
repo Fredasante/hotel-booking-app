0413e4acfe2ccda3374b2378a1d77f84f72ec670
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-07-02 11:28:42 +0000

    Created & Run the Server

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", (req: Request, res: Response) => {
+  res.send("Jellow");
+});
+
+app.listen(7000, () => {
+  console.log("Server running on localhost:7000");
+});
