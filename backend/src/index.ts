648903354b1b2fecdea54d5f3095a290a45e6b05
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-07-02 12:33:52 +0000

    Mongodb Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 const app = express();
 app.use(express.json());
