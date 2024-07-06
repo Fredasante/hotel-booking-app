b0a472f108384fee293d8c8afe74e7a2b0740ee5
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-07-06 12:24:26 +0000

    Modified Login Functionality, user doc added

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -82,7 +82,9 @@ export const login = async (req: Request, res: Response) => {
       maxAge: 86400000,
     });
 
-    res.status(200).json({ userId: user._id });
+    const { password: pass, ...rest } = user.toObject();
+
+    res.status(200).json(rest);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong!" });
