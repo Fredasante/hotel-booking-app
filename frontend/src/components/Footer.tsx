563647187764558c075f435452b72a915153d2a3
Alfred Apenteng <alfredapenteng6996@gmail.com>
2024-08-19 16:22:45 +0000

    update: footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#003B95] text-white py-10 px-5 md:px-10 lg:px-14">
+    <footer className="bg-gray-100 text-slate-700 py-10 px-5 md:px-10 lg:px-14">
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between md:items-center">
           <div className="mb-6 md:mb-0">
-            <h2 className="text-3xl font-bold">Bookly</h2>
+            <h2 className="text-2xl font-bold">Bookings</h2>
             <p className="mt-2">
               Your perfect home base for every special trip
             </p>
           </div>
           <div className="flex space-x-4">
             <a
-              className="hover:text-gray-300 transition"
+              className="hover:text-gray-500 transition"
               href="#"
               target="_blank"
               rel="noopener noreferrer"
@@ -21,7 +21,7 @@ const Footer = () => {
               <i className="fab fa-twitter"></i>
             </a>
             <a
-              className="hover:text-gray-300 transition"
+              className="hover:text-gray-500 transition"
               href="#"
               target="_blank"
               rel="noopener noreferrer"
@@ -29,7 +29,7 @@ const Footer = () => {
               <i className="fab fa-facebook-f"></i>
             </a>
             <a
-              className="hover:text-gray-300 transition"
+              className="hover:text-gray-500 transition"
               href="#"
               target="_blank"
               rel="noopener noreferrer"
@@ -40,12 +40,12 @@ const Footer = () => {
         </div>
         <div className="mt-6 border-t border-gray-400 pt-6">
           <div className="flex flex-col md:flex-row justify-between md:items-center">
-            <p>&copy; 2024 Bookly. All Rights Reserved.</p>
-            <div className="mt-4 md:mt-0">
-              <Link to="/" className="hover:text-gray-400 transition mx-2">
+            <p className="text-sm">&copy; 2024 Bookly. All Rights Reserved.</p>
+            <div className="mt-4 md:mt-0 text-sm">
+              <Link to="/" className="hover:text-gray-500 transition mx-2">
                 Terms of Service
               </Link>
-              <Link to="/" className="hover:text-gray-300  transition mx-2">
+              <Link to="/" className="hover:text-gray-500  transition mx-2">
                 Privacy Policy
               </Link>
             </div>
